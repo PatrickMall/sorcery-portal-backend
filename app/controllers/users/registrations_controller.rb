f098a391cf18a8fbaf5a91dd591a589606365157
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-05 23:26:34 +0100

    add user update details route

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,30 +3,46 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    if resource.update(account_update_params)
+      render json: {
+        status: { code: 200, message: "User details updated successfully." },
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }, status: :ok
+    else
+      render json: {
+        status: { code: 422, message: "User details couldn't be updated successfully. #{resource.errors.full_messages.to_sentence}" }
+      }, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number])
+  end
 
   def respond_with(resource, _opts = {})
     if request.method == "POST" && resource.persisted?
       render json: {
-        status: {code: 200, message: "Signed up sucessfully."},
+        status: { code: 200, message: "Signed up successfully." },
         data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
       }, status: :ok
     elsif request.method == "DELETE"
       render json: {
-        status: { code: 200, message: "Account deleted successfully."}
+        status: { code: 200, message: "Account deleted successfully." }
       }, status: :ok
     else
       render json: {
-        status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+        status: { code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
       }, status: :unprocessable_entity
     end
   end
-end
-
-def configure_sign_up_params()
-  devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :phone_number])
-end
-
-def configure_account_update_params()
-  devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :phone_number])
 end

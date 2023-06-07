8e7422f28bcd556f040b721755ee5ed843e55e1d
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-07 20:38:49 +0100

    revert to local host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://portal.sorcery.design"
+    origins "http://localhost:3000"
 
     resource "*",
       headers: :any,
       expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: :true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 
 end

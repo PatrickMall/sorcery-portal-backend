338ef7a2c3dd977b99339d3d41908e6779240ae1
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-07 20:50:07 +0100

    add sorcery to cors list

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
   allow do
-    origins "http://localhost:3000"
+    origins "https://portal.sorcery.design"
 
     resource "*",
       headers: :any,

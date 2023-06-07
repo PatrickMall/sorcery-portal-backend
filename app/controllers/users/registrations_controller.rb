6b436a7e7bf58408975accb2cf262856640701c5
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-07 17:07:16 +0100

    add sort feature

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,22 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
-  def update
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-
-    if resource.update(account_update_params)
-      render json: {
-        status: { code: 200, message: "User details updated successfully." },
-        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-      }, status: :ok
-    else
-      render json: {
-        status: { code: 422, message: "User details couldn't be updated successfully. #{resource.errors.full_messages.to_sentence}" }
-      }, status: :unprocessable_entity
-    end
-  end
-
-  private
+private
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
@@ -38,6 +23,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     elsif request.method == "DELETE"
       render json: {
         status: { code: 200, message: "Account deleted successfully." }
+      }, status: :ok
+    elsif request.method == "PATCH" && resource.persisted?
+      render json: {
+        status: { code: 200, message: "Account updated successfully."}
       }, status: :ok
     else
       render json: {

0b415c60eb0e945a0ee4a289bd7d4f1c457dd306
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-07 20:53:06 +0100

    swap url to sorcery

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,14 +7,6 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
-
-    resource "*",
-      headers: :any,
-      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-  allow do
     origins "https://portal.sorcery.design"
 
     resource "*",

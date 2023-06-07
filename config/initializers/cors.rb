cbe0de6e7ac9e53bc7b9d5b56f7d62b9b965a072
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-07 20:47:50 +0100

    add sorcery to allowed urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,5 +14,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+  allow do
+    origins "http://localhost:3000"
+
+    resource "*",
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 
 end

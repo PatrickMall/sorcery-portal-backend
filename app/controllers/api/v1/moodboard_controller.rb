f812908846167ff86df77e64cd803ff9bacb291a
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-08 13:47:32 +0100

    sort moodboard controller

--- app/controllers/api/v1/moodboard_controller.rb
+++ app/controllers/api/v1/moodboard_controller.rb
@@ -12,37 +12,30 @@ module Api
   
         def create
             if current_user 
-          mood= Answer.new(answer_params)
-          @answer.user_id= current_user.id
-                 if @answer.save
-                    render json: {status: 'SUCCESS', message: 'Answer is saved', data:@answer}, status: :ok
+          moodboard = Moodboard.new(moodboard_params)
+          moodboard.user_id= current_user.id
+                 if moodboard.save
+                    render json: {status: 'SUCCESS', message: 'Image is saved', data:moodboard}, status: :ok
                 else
-                    render json: {status: 'Error', message: 'Answer is not saved', data:@answer.errors}, status: :unprocessable_entity
+                    render json: {status: 'Error', message: 'Image is not saved', data:moodboard.errors}, status: :unprocessable_entity
                 end
             end
         end
   
         def destroy
             if current_user
-                question_number = params[:id]
-                @answer = current_user.answers.find_by(question_id: question_number)
-                @answer.user_id= current_user.id
-            @answer.destroy
-          render json: {status: 'SUCCESS', message: 'Answer successfully deleted', data:@answer}, status: :ok
+                image_number = params[:id]
+                moodboard = current_user.answers.find_by(id: image_number)
+                moodboard.user_id= current_user.id
+            moodboard.destroy
+          render json: {status: 'SUCCESS', message: 'Image successfully deleted', data:moodboard}, status: :ok
             end
         end
 
-        def destroy_all
-            if current_user
-            @answers = current_user.answers
-            @answers.destroy_all
-            render json: {status: 'SUCCESS', message: 'Answers were all successfully deleted', data:@answers}, status: :ok
-          end
-        end
   
         private
-          def answer_params
-            params.permit(:question_id, :answer)
+          def moodboard_params
+            params.permit(:url)
           end
   
       end

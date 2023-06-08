247327734bb6e9add1ffc7bccae622716f52490e
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-06-08 12:08:07 +0100

    remove broken update method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -24,11 +24,6 @@ private
       render json: {
         status: { code: 200, message: "Account deleted successfully." }
       }, status: :ok
-    elsif request.method == "PATCH" && resource.persisted?
-      render json: {
-        status: { code: 200, message: "Account updated successfully."}
-        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-      }, status: :ok
     else
       render json: {
         status: { code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
